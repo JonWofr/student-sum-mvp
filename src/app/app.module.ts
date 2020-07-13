3efa3d86231a871c0b8b06564917b5ee537c92de
Nico Burkart <nb094@hdm-stuttgart.de>
2020-07-13 16:19:11 +0200

    added buy-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ContactComponent } from './views/contact/contact.component';
 import { CompanyInformationComponent } from './views/company-information/company-information.component';
 import { AdvantageComponent } from './components/advantage/advantage.component';
 import { SearchfieldComponent } from './components/searchfield/searchfield.component';
+import { BuyFormComponent } from './views/buy-form/buy-form.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { SearchfieldComponent } from './components/searchfield/searchfield.compo
     CompanyInformationComponent,
     AdvantageComponent,
     SearchfieldComponent,
+    BuyFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
