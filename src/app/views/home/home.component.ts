89e468c09bb9a30fe6850715c30a5f0d52a9998d
Nico Burkart <nb094@hdm-stuttgart.de>
2020-07-13 15:04:43 +0200

    styling home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  onChangeSelectValue(value: string): void {
+    if (value === 'hdm') {
+      this.router.navigateByUrl('search-results/hdm');
+    }
   }
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
 }
